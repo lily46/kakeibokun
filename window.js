8c04ca09a790f8e25024ca0623adc84f469cf1ad
lily46 <info@46flare.net>
2021-03-07 21:09:48 +0900

    他の月を読む

--- window.js
+++ window.js
@@ -2,41 +2,78 @@ const fs = require('fs');
 const csv = require('csv');
 
 let mainWindow = null;
-console.log('初期データ');
 var now = new Date();
 
-let global_data = fs.createReadStream('202102.csv', {encoding: 'utf-8'});
-//処理（跡でpipeに食べさせる）
-const parser = csv.parse((error, data) => {
-    //変換後の配列を格納
-    let total = 0;
+function GetCsvList(callback){
+    fs.readdir('.', function(err, files){
+        if (err) throw err;
+        var fileList = [];
+        files.filter(function(file){
+            return fs.statSync(file).isFile() && /.*\.csv$/.test(file); //絞り込み
+        }).forEach(function (file) {
+            fileList.push(file);
+        });
+        callback(fileList);
+    });
+}
 
-    $('#kakeibo').html('');
+function MakeTable(id){
+    let csvData = {};
+    //処理（跡でpipeに食べさせる）
+    const parser = csv.parse((error, data) => {
 
-    //内容出力
-    console.log('初期データ');
-    console.log(data);
+        //変換後の配列を格納
+        let total = 0;
 
-    //ループしながら１行ずつ処理
-    data.forEach((element, index, array) => {
-        total += Number(element[1]);
-        const tags = element[2].split(' ');
-        let tagStr = '';
-        tags.forEach(
-            element => tagStr += '<span class="badge badge-secondary">' + element + '</span> '
-        );
+        $('#kakeibo').html('');
 
-        $('#kakeibo').append('<tr><td>' + element[0] + '</td><td>' + element[1] + '</td><td>' + tagStr +'</td></tr>');
+        //内容出力
+        console.log(data);
+
+        data.forEach((element, index, array) => {
+            total += Number(element[1]);
+            const tags = element[2].split(' ');
+            let tagStr = '';
+            tags.forEach(
+                element => tagStr += '<span class="badge badge-secondary">' + element + '</span> '
+            );
+
+            $('#kakeibo').append('<tr><td>' + element[0] + '</td><td>' + element[1] + '</td><td>' + tagStr +'</td></tr>');
+        })
+        console.log('total ' + total);
+        console.log('ave ' + (total / data.length));
+        $('#total').html(total + " 円");
+        let ave = Math.round(total / now.getDate() * 10) / 10;
+        $('#average').html(ave + " 円");
+
     })
-    console.log('total ' + total);
-    console.log('ave ' + (total / data.length));
-    $('#total').html(total + " 円");
-    let ave = Math.round(total / now.getDate() * 10) / 10;
-    $('#average').html(ave + " 円");
+    csvData = fs.createReadStream(id, {encoding: 'utf-8'});
+    csvData.pipe(parser);  
+}
 
-})
+$(function(){
+    $('#make').on('click', function() {
+        var id = '202102.csv';
+        MakeTable(id);
+    });
 
-$('#make').on('click', function() {
-    global_data.pipe(parser);  
+    $(document).on("click", '.dropdown-menu .dropdown-item', function(){
+        var visibleItem = $('.dropdown-toggle', $(this).closest('.dropdown'));
+
+        var id = $(this).attr('value');
+        console.log(id);
+        MakeTable(id);
+    });
 });
 
+MakeTable('202102.csv');
+
+
+var func = function(months){
+    var monthStr = "";
+    months.forEach(function(element){
+        monthStr += '<li><button class="dropdown-item clear-decoration" value="' + element + '">' + element + '</button></li>';
+    });
+    $('#months').html(monthStr);
+}
+GetCsvList(func);
